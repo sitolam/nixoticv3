2b12c1fa9ca38348662a2b3536669c6eef7ed694
Aylur <demstof@gmail.com>
2024-04-11 20:18:31 +0200

    Fixed firefox css take two

--- home/programs/browsers/firefox.nix
+++ home/programs/browsers/firefox.nix
@@ -22,24 +22,24 @@
 
   programs.firefox = {
     enable = true;
-    profiles.default = {
-      name = "Default";
-      settings = {
-        "browser.tabs.loadInBackground" = true;
-        "widget.gtk.rounded-bottom-corners.enabled" = true;
-        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-        "svg.context-properties.content.enabled" = true;
-        "gnomeTheme.hideSingleTab" = true;
-        "gnomeTheme.bookmarksToolbarUnderTabs" = true;
-        "gnomeTheme.normalWidthTabs" = false;
-        "gnomeTheme.tabsAsHeaderbar" = false;
-      };
-      # userChrome = ''
-      #   @import "firefox-gnome-theme/userChrome.css";
-      # '';
-      # userContent = ''
-      #   @import "firefox-gnome-theme/userContent.css";
-      # '';
-    };
+    # profiles.default = {
+    #   name = "Default";
+    #   settings = {
+    #     "browser.tabs.loadInBackground" = true;
+    #     "widget.gtk.rounded-bottom-corners.enabled" = true;
+    #     "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+    #     "svg.context-properties.content.enabled" = true;
+    #     "gnomeTheme.hideSingleTab" = true;
+    #     "gnomeTheme.bookmarksToolbarUnderTabs" = true;
+    #     "gnomeTheme.normalWidthTabs" = false;
+    #     "gnomeTheme.tabsAsHeaderbar" = false;
+    #   };
+    #   # userChrome = ''
+    #   #   @import "firefox-gnome-theme/userChrome.css";
+    #   # '';
+    #   # userContent = ''
+    #   #   @import "firefox-gnome-theme/userContent.css";
+    #   # '';
+    # };
   };
 }
