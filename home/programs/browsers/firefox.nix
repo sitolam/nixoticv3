90e1f1b5e4e5d134ddd219dbb475c85063c2ca56
Aylur <demstof@gmail.com>
2024-04-12 13:54:29 +0200

    Added firefox theme

--- home/programs/browsers/firefox.nix
+++ home/programs/browsers/firefox.nix
@@ -1,45 +1,32 @@
-{inputs, ...}: {
+{inputs, pkgs, ...}: {
+
   home = {
     sessionVariables.BROWSER = "firefox";
 
-    # file."firefox-gnome-theme" = {
-    #   target = ".mozilla/firefox/default/chrome/firefox-gnome-theme";
-    #   source = inputs.firefox-gnome-theme;
-    # };
-
-    file."chrome" = {
-      target = ".mozilla/firefox/default/chrome";
-      source = "./css/chrome";
-    };
-
-    file."user.js" = {
-      target = ".mozilla/firefox/default/user.js";
-      source = "./css/user.js";
+    file."firefox-one" = {
+      target = ".mozilla/firefox/default/chrome/firefox-one";
+      source = ./css/firefox-one;
     };
 
 
-  };
-
-  programs.firefox = {
-    enable = true;
-    # profiles.default = {
-    #   name = "Default";
-    #   settings = {
-    #     "browser.tabs.loadInBackground" = true;
-    #     "widget.gtk.rounded-bottom-corners.enabled" = true;
-    #     "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-    #     "svg.context-properties.content.enabled" = true;
-    #     "gnomeTheme.hideSingleTab" = true;
-    #     "gnomeTheme.bookmarksToolbarUnderTabs" = true;
-    #     "gnomeTheme.normalWidthTabs" = false;
-    #     "gnomeTheme.tabsAsHeaderbar" = false;
-    #   };
-    #   # userChrome = ''
-    #   #   @import "firefox-gnome-theme/userChrome.css";
-    #   # '';
-    #   # userContent = ''
-    #   #   @import "firefox-gnome-theme/userContent.css";
-    #   # '';
-    # };
+    programs.firefox = {
+      enable = true;
+      profiles.default = {
+        name = "Default";
+        settings = {
+          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+          "svg.context-properties.content.enabled" = true;
+          "layout.css.color-mix.enabled" = true;
+          "browser.tabs.delayHidingAudioPlayingIconMS" = 0;
+          "layout.css.backdrop-filter.enabled" = true;
+          "browser.newtabpage.activity-stream.improvesearch.handoffToAwesomebar" = false;
+          "privacy.userContext.enabled" = true;
+          "privacy.userContext.ui.enabled" = true;
+          "privacy.userContext.longPressBehavior" = 2;
+        };
+        userChrome = ./css/userChrome.css;
+        userContent = ./css/userContent.css;
+      };
+    };
   };
 }
